540a407b3dafc855ea46b76ce4aacc5c627ed93c
BrutusD <4570820+BrutusD@users.noreply.github.com>
2019-05-03 19:28:42 +0200

    I replaced the Method of Errorhandling. Before I used the throws command, but since I realised, that throwing an error in an asyncronus program thread is foolish, I changed my way

--- EventManager/PresetsTableViewController.swift
+++ EventManager/PresetsTableViewController.swift
@@ -68,15 +68,17 @@ class PresetsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the event from the event store
-            do {
-                try eventHelper.removeCalenderEventCorrspondingTo(presets[indexPath.row])
-            } catch {
-                print("An error occured while deleting an event: \(error)")
+            eventHelper.removeCalenderEventCorrspondingTo(presets[indexPath.row]) { error in
+                if error == nil {
+                    // Delete the row from the data source.
+                    self.presets.remove(at: indexPath.row)
+                    self.saveEventCreationPresets()
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                } else {
+                    self.handleNotAuthorizedStatus(error!)
+                }
             }
-            // Delete the row from the data source.
-            presets.remove(at: indexPath.row)
-            saveEventCreationPresets()
-            tableView.deleteRows(at: [indexPath], with: .fade)
+
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
@@ -127,6 +129,21 @@ class PresetsTableViewController: UITableViewController {
             let selectedPreset = presets[indexPath.row]
             eventDetailViewController.eventCreationPreset = selectedPreset
             
+            
+        case "BackToDetail":
+            guard let eventDetailViewController = segue.destination as? EventCreationPresetViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let preset = sender as? EventCreationPreset else {
+                fatalError("Unexpected sender: \(String(describing: sender))")
+            }
+//            guard let oldEventDetailViewController = sender as? EventCreationPresetViewController else {
+//                fatalError("Unexpected origin: \(String(describing: sender))")
+//            }
+            
+            eventDetailViewController.eventCreationPreset = preset //oldEventDetailViewController.eventCreationPreset
+            
         default:
             fatalError("Unexpected segue Identifier: \(String(describing: segue.identifier))")
         }
@@ -140,67 +157,79 @@ class PresetsTableViewController: UITableViewController {
      - Parameter sender:
     */
     @IBAction func unwindToPresetsList(sender: UIStoryboardSegue) {
-        var newCalendarEventIdentifier: String? = nil
+//        var newCalendarEventIdentifier: String? = nil
         
         if let sourceViewControler = sender.source as? EventCreationPresetViewController, let preset = sourceViewControler.eventCreationPreset {
             
             // If one of the table view rows is selected
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 // Update an existing calender event with the information from the preset.
-                do {
-                    try eventHelper.editCalenderEventCorrespondingTo(preset)
-                    
-                    // And update the preset in the array.
-                    presets[selectedIndexPath.row] = preset
-                    tableView.reloadRows(at: [selectedIndexPath], with: .none)
-                    
-                } catch EventHelperError.unableToRetrieveEvent(let unrecognizedID) {
-                    print("The event could not be updated. No event found with the ID: \(unrecognizedID)")
-                } catch {
-                    print(error)
+                eventHelper.editCalenderEventCorrespondingTo(preset) { error in
+                    if error == nil {
+                        // And update the preset in the array.
+                        self.presets[selectedIndexPath.row] = preset
+                        self.tableView.reloadRows(at: [selectedIndexPath], with: .none)
+                        
+                        // Save the presets.
+                        self.saveEventCreationPresets()
+                    } else {
+                        self.handleNotAuthorizedStatus(error!)
+                    }
                 }
-            }
-            else {
+            } else {
                 // If no table view row is selected create a new calendar event that corresponds to the preset.
-                do  {
-                    newCalendarEventIdentifier = try eventHelper.createCalendarEvent(for: preset)
-                } catch EventHelperError.authorisationDenied {
-                    print("Access to the EKEventStore was denied.")
-                    // TODO: Let the user know, we need permission to acces the calender for the app to work.
-                } catch EventHelperError.authorisationRestricted {
-                    print("Users Access to calender is restricted.")
-                    // TODO: Let the user know the app can not work since his access to the calendar is restricted and what that means.
-                } catch {
-                    print("Unexpected error \(error).")
+                eventHelper.createCalendarEvent(for: preset) { (identifier, error) in
+                    if error == nil {
+                        preset.identifierForEvent = identifier
+                        
+                        // Add the new event creation preset to the preset array.
+                        let newIndexPath = IndexPath(row: self.presets.count, section: 0)
+                        
+                        self.presets.append(preset)
+                        self.tableView.insertRows(at: [newIndexPath], with: .automatic)
+                        
+                        // Save the presets.
+                        self.saveEventCreationPresets()
+                    } else {
+                        self.handleNotAuthorizedStatus(error!)
                 }
-                
-                // If the retruned identifier is nil, we will have an orphan calender entry, which is not good
-                guard newCalendarEventIdentifier != nil else {
-                    fatalError("Could not retrieve event id.")
-                }
-                // TODO: The event helper should assure, that he always returns a string.
-
-                
-                // Assing the EKevent identifier to the preset.
-                preset.identifierForEvent = newCalendarEventIdentifier
-                
-                // Add the new event creation preset to the preset array.
-                let newIndexPath = IndexPath(row: presets.count, section: 0)
-                
-                presets.append(preset)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
-            // Save the presets.
-            saveEventCreationPresets()
+//                } catch EventHelperError.authorisationDenied {
+//                    print("Access to the EKEventStore was denied.")
+//
+//                    // perform a segue back to the original sender
+//                    DispatchQueue.main.async {
+//                        self.performSegue(withIdentifier: "BackToDetail", sender: preset)
+//                    }
+//
+//
+//                    // TODO: Let the user know, we need permission to acces the calender for the app to work.
+//                } catch EventHelperError.authorisationRestricted {
+//                    print("Users Access to calender is restricted.")
+//                    // TODO: Let the user know the app can not work since his access to the calendar is restricted and what that means.
+//                } catch {
+//                    print("Unexpected error \(error).")
+//                }
+                
+//                // If the retruned identifier is nil, we will have an orphan calender entry, which is not good
+//                guard newCalendarEventIdentifier != nil else {
+//                    fatalError("Could not retrieve event id.")
+//                }
+//                // TODO: The event helper should assure, that he always returns a string.
+
+//
+//                // Assing the EKevent identifier to the preset.
+//                preset.identifierForEvent = newCalendarEventIdentifier
+            }
         }
     }
     
     /// - NOTE: I found the explanation to trigger a unwind segue entirely from code [here](https://www.andrewcbancroft.com/2015/12/18/working-with-unwind-segues-programmatically-in-swift/#trigger)
-    /// Ofcourse the concrete implementation is mine.
+    /// Of course the concrete implementation is mine.
     @IBAction func unwindToPresetListAndDelete(sender: UIStoryboardSegue) {
         var calendarEventIdentifier: String? = nil
         
-        // Gehe sicher das die segue von einerm EventCreationPresetViewController kommt, und bekomme eine referenz fpr das preset object.
+        // Gehe sicher dass die segue von einerm EventCreationPresetViewController kommt, und bekomme eine referenz fpr das preset object.
         if let sourceViewControler = sender.source as? EventCreationPresetViewController, let preset = sourceViewControler.eventCreationPreset {
             
             // Make shure one of the table view rows is selected
@@ -215,24 +244,33 @@ class PresetsTableViewController: UITableViewController {
                 return
             }
             
-            do {
-                // Delete the event from the calender.
-                try eventHelper.removeCalenderEventCorrspondingTo(preset)
-                
-                // Remove the preset from the array and reload the table view row
-                presets.remove(at: selectedIndexPath.row)
-                tableView.reloadData()//reloadRows(at: [selectedIndexPath], with: .none)
-            } catch {
-                print(error)
+            // Delete the event from the calender.
+            eventHelper.removeCalenderEventCorrspondingTo(preset) { error in
+                if error == nil {
+                    // Remove the preset from the array and reload the table view row
+                    self.presets.remove(at: selectedIndexPath.row)
+                    self.tableView.reloadData()//reloadRows(at: [selectedIndexPath], with: .none)
+                } else {
+                    self.handleNotAuthorizedStatus(error!)
+                }
             }
         }
-        
-
     }
     
     
     // MARK: - Private Methods
     /// Fills the event creation presets array with three event creation presets and creates corresponding calender events.
+    private func handleNotAuthorizedStatus(_ error: EventHelperError) {
+        switch error {
+        case .authorisationDenied:
+            print("please authorise so I can do stuff with your calender")
+        case .authorisationRestricted:
+            print("please tell your parents to let me do stuff")
+        default:
+            print("something else went wrong! HELP!")
+        }
+    }
+    
     private func loadSampleEventCreationPresets() {
         
         // Create some time Intervals for three dates for three sample Events
@@ -260,11 +298,12 @@ class PresetsTableViewController: UITableViewController {
             
             // Create events for all presets
             for preset in presets {
-                do {
-                    let newIdentifier = try eventHelper.createCalendarEvent(for: preset)
-                    preset.identifierForEvent = newIdentifier
-                } catch {
-                    print("Unable to create event \"\(preset.title)\": \(error)")
+                eventHelper.createCalendarEvent(for: preset) { (identifier, error) in
+                    if error == nil {
+                        preset.identifierForEvent = identifier
+                    } else {
+                        self.handleNotAuthorizedStatus(error!)
+                    }
                 }
             }
         }
@@ -289,23 +328,29 @@ class PresetsTableViewController: UITableViewController {
         print("HURRAR!")
         
         for preset in presets {
-            do {
-                if try eventHelper.needsToUpdate(preset) {
-                   try eventHelper.update(preset)
+            eventHelper.updateIfNeeded(preset) { error in
+                if error == nil {
+                    self.saveEventCreationPresets()
+                    self.tableView.reloadData()
+                
+                } else {
+                    switch error! {
+                    case .unableToRetrieveEvent(identifier: _):
+                        // If the evnet could not be retrieved with the identifier stored in the preset, the event probably has been deleted.
+                        // Mark the preset as deleted by adding a suffix
+                        let deleteSuffix = " - deleted!"
+                        if !preset.title.hasSuffix(deleteSuffix) {
+                            preset.title += deleteSuffix
+                            // TODO: Move the preset to a bin like destinatiion where it could be reviewed and accessed by the user.
+                        }
+                    default:
+                            self.handleNotAuthorizedStatus(error!)
+                    }
+ 
+                
                 }
-            } catch EventHelperError.unableToRetrieveEvent(_) {
-                // If the evnet could not be retrieved with the identifier stored in the preset, the event probably has been deleted.
-                // Mark the preset as deleted by adding a suffix
-                let deletSuffix = " - deleted!"
-                if !preset.title.hasSuffix(deletSuffix) {
-                    preset.title += " - deleted!"
-                    // TODO: Move the preset to a garbage destinatiion where it could be reviewed and accessed by the user.
-                }
-            } catch {
-                print(error)
             }
         }
-        saveEventCreationPresets()
-        tableView.reloadData()
     }
 }
+
